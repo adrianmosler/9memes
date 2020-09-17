b32b7c10ad78262dfd8c59ba7235db6b2fe2c744
NanoWi <mariano.wilson@est.fi.uncoma.edu.ar>
2020-09-16 22:12:41 -0300

    Reordenamos las paginas, eliminamos cosas de mas y acomdamos el Nav Bar

--- app/9memes-app/src/App.js
+++ app/9memes-app/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Navegador from "./components/layouts/nav_bar/BarraNavegacion";
+import Navegador from "./components/layouts/nav_bar/Nav_bar";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import Principal from "./components/pages/Principal";
-import ListaMeme from "./components/pages/ListaMeme";
 import FreshNine from "./components/pages/FreshNine/FreshNine";
 import ImpopularNine from "./components/pages/ImpopularNine/ImpopularNine";
+import MostPopular from "./components/pages/MostPopular/MostPopular";
+
+
 
 class App extends React.Component {
   render() {
@@ -14,11 +15,10 @@ class App extends React.Component {
         <Navegador />
         <div id="contenedorDeLista">
           <Switch>
-            <Route path="/All" component={Principal}></Route>
-            <Route path="/FreshNine" exact><FreshNine/></Route>
-            <Route path="/UpcomingNine" component={ListaMeme} exact></Route>
-            <Route path="/MostPopular" component={ListaMeme} exact></Route>
-            <Route path="/ImpopularNine" exact><ImpopularNine/></Route>
+            <Route path="/Home" exact><FreshNine/></Route>
+            <Route path="/Categorys" exact></Route>
+            <Route path="/MostPopular"exact><MostPopular/></Route>
+            <Route path="/Impopular" exact><ImpopularNine/></Route>
           </Switch>
         </div>
       </div>
---
+++ app/9memes-app/src/components/layouts/nav_bar/Nav_bar.js
@@ -0,0 +1,78 @@
+import logo from "./9memes.jpeg";
+import React from "react";
+import "./BarraNavegacion.css";
+import {
+  Navbar,
+  Image,
+  NavbarToggle,
+  NavbarCollapse,
+  Container,
+  NavDropdown,
+  NavbarBrand,
+} from "react-bootstrap";
+import { GiFlame, GiLindenLeaf } from "react-icons/gi";
+import { AiOutlineThunderbolt } from "react-icons/ai";
+import { FaPoo } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+function Nav_bar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" id="nav">
+      <sector_img logo={logo} />
+
+      <Container className="justify-content-center">
+        <ItemNav url="/Home" icons="" name="Home" />
+        <ItemNav url="/MostPopular" icons={<GiFlame />} name="Most Popular" />
+        <ItemNav url="/Impopular" icons={<FaPoo />} name="Impopular" />
+
+        <NavDropdown title="Category" id="collasible-nav-dropdown">
+          <ItemNav url="/Funny" icons={<AiOutlineThunderbolt />} name="Funny" />
+          <ItemNav
+            url="/Animals"
+            icons={<AiOutlineThunderbolt />}
+            name="Animals"
+          />
+          <ItemNav
+            url="/Argentina"
+            icons={<AiOutlineThunderbolt />}
+            name="Argentina"
+          />
+        </NavDropdown>
+      </Container>
+    </Navbar>
+  );
+}
+
+function ItemNav(props) {
+  return (
+    <NavLink
+      className="navItem"
+      to={props.url}
+      activeStyle={{
+        color: "#2EFE2E",
+      }}
+      style={{
+        color: "#04B404",
+      }}
+    >
+      {props.icons}
+      {props.name}
+    </NavLink>
+  );
+}
+
+function sector_img(props) {
+  return (
+    <NavbarBrand>
+      <Image
+        src={props.logo}
+        alt="logo"
+        width="90"
+        height="90"
+        roundedCircle
+        variant="top"
+      />
+    </NavbarBrand>
+  );
+}
+export default Nav_bar;
