5acc072a64571ad2b8e4e23b12cd571d767184e0
Cesar Ovidio Gutierrez <cesar.gutierrez@est.fi.uncoma.edu.ar>
2020-09-18 17:57:28 -0300

    fin

--- app/9memes-app/src/components/helper/helper.js
+++ app/9memes-app/src/components/helper/helper.js
@@ -14,7 +14,8 @@ function GenericList(props) {
           <CardMeme
             direccion={elemento.img}
             key={elemento._id}
-            title={elemento.title}  categoria ={elemento.category}
+            title={elemento.title}
+            categoria={elemento.category}
           />
         ))}
       </CardColumns>
@@ -43,11 +44,10 @@ function Sector_Header(props) {
   );
 }
 function Sector_Footer(props) {
-  let salida="";
-   props.category.forEach(elemento=>salida += elemento.name+'\n');
-   
+  let salida = "";
+  props.category.forEach((elemento) => (salida += elemento.name + "\n"));
+
   return (
-<<<<<<< HEAD
     <Card.Footer>
       {props.category}
       {props.like ? props.like.length : 0}
@@ -58,27 +58,6 @@ function Sector_Footer(props) {
       <Button className="btn-circle" variant="danger">
         <AiOutlineDislike />
       </Button>{" "}
-=======
-       <Card.Footer >
-
-      <h5 style={{textAlign:"right"}}>
-        {""}
-        <h6 style={{textAlign:"left"}}>
-          {salida}
-        </h6>
-             
-        12
-        
-        <Button className="btn-circle" variant="success" >
-          <AiOutlineLike />
-          
-        </Button>{" "}
-        -12
-        <Button className="btn-circle" variant="danger">
-          <AiOutlineDislike />
-        </Button>{" "}
-      </h5>
->>>>>>> 3d68cacf3fa27db8b23d21310cbbe31de9f24a3f
     </Card.Footer>
   );
 }
